9c37a5c73cc075db7e06d20aa97ac408a59fe079
Christopher Manna <cmanna285@gmail.com>
2018-08-13 18:26:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {}
 function updateObjectWithKeyAndValue(object,key,value) {
-  console.log(object);
+  console.log(recipes);
   return Object.assign(recipes, object, {key: value})
   // return object
 }
